52cd2ee623132751bb7285177339966637ee3e3d
Pedro Gryzinsky <pedro.gryzinsky@zrp.com.br>
2019-08-20 20:24:46 -0300

    fix(config/environment): added docs for env vars and missing default metadata for logger

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,5 +1,19 @@
-require("dotenv").config()
+/**
+ * Provides constants on initialization for managing the environment
+ *
+ * @module config/environment
+ */
 
+// Dependencies
+const os = require("os")
+
+/**
+ * Configures how a task should be launch in the cluster
+ *
+ * @static
+ * @constant
+ * @type {Object<string, any>}
+ */
 const taskParams = {
   cluster: process.env.CLUSTER,
   taskDefinition: process.env.TASK_DEFINITION,
@@ -13,15 +27,67 @@ const taskParams = {
   },
 }
 
-const awsAuthParams = {
+/**
+ * Returns true when NODE_ENV is set to production
+ *
+ * @static
+ * @constant
+ * @type {boolean}
+ * */
+const isProd = process.env.NODE_ENV === "production"
+
+/**
+ * Globally configured timezone
+ * Defaults to America/Sao_Paulo
+ *
+ * @static
+ * @constant
+ * @type {string}
+ */
+const timezone = process.env.TZ || "America/Sao_Paulo"
+
+/**
+ * Defines (in milliseconds) the time between request retries
+ *
+ * @static
+ * @constant
+ * @type {number}
+ */
+const retryDelay = process.env.RETRY_DELAY || 5000
+
+/**
+ * Defines the maximum number of attempts before considering
+ * a request unfulfilled.
+ *
+ * @static
+ * @constant
+ * @type {number}
+ */
+const maxAttempts = process.env.MAX_ATTEMPTS || 5
+
+/**
+ * Default metadata for logging services
+ *
+ * @static
+ * @constant
+ * @type {Object.<string, any>}
+ */
+const defaultMeta = {
+  service: "scraper",
+  host: os.hostname(),
+  arch: os.arch(),
+  cpus: os.cpus().length,
+  platform: os.platform(),
+  totalmem: parseInt(os.totalmem() / 1000.0 ** 2) + "MB",
+  category: "no-category",
 }
 
-const taskPath = process.env.TASK_PATH
-const taskRequestMethod = process.env.TASK_REQUEST_METHOD
-
+// Exports
 module.exports = {
   taskParams,
-  awsAuthParams,
-  taskPath,
-  taskRequestMethod
+  isProd,
+  timezone,
+  retryDelay,
+  maxAttempts,
+  defaultMeta,
 }
